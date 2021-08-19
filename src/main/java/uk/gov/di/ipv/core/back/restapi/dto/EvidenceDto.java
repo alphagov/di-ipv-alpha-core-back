9ad72daf456da325b8b5bb849d5b7312b849b339
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2021-08-19 14:05:58 +0100

    BAU: Passing scores from the core-front for testing purposes.
    - Allowing to pass the scores of the evidence via core-front.

--- src/main/java/uk/gov/di/ipv/core/back/restapi/dto/EvidenceDto.java
+++ src/main/java/uk/gov/di/ipv/core/back/restapi/dto/EvidenceDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import uk.gov.di.ipv.core.back.domain.data.BundleScores;
 import uk.gov.di.ipv.core.back.domain.data.EvidenceType;
 import uk.gov.di.ipv.core.back.domain.data.IdentityEvidence;
+import uk.gov.di.ipv.core.back.domain.gpg45.EvidenceScore;
 
 import java.util.UUID;
 
@@ -12,13 +13,17 @@ public class EvidenceDto {
     private UUID evidenceId;
     private EvidenceType type;
     private Object evidenceData;
+
+    // Only passing bundle scores here temporarily for mocking.
     private BundleScores bundleScores;
+    private EvidenceScore evidenceScore;
 
     public static EvidenceDto toDto(IdentityEvidence identityEvidence) {
         var dto = new EvidenceDto();
         dto.setType(identityEvidence.getType());
         dto.setEvidenceId(identityEvidence.getUuid());
         dto.setEvidenceData(identityEvidence.getEvidenceData());
+        dto.setEvidenceScore(identityEvidence.getEvidenceScore());
 
         return dto;
     }
